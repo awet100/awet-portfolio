891e1565d1286707897d3123c13c2e530435000d
Awet Tesfay <whitezscope@gmail.com>
2021-05-05 22:16:14 +0200

    able to send fack email

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -15,4 +19,28 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    #[Route('/email', name: 'email')]
+    public function sendEmail(MailerInterface $mailer, Request $request): Response
+    {
+        if ($request->request->get('email') && $request->request->get('subject') && $request->request->get('message')) {
+
+            $message = $request->request->get('message');
+            $email = (new Email())
+                ->from($request->request->get('email'))
+                ->to('you@example.com')
+                ->priority(Email::PRIORITY_HIGH)
+                ->subject($request->request->get('subject'))
+                ->text($message)
+                ->html("<p>$message</p>");
+
+            $mailer->send($email);
+        }
+
+        return $this->redirectToRoute('home');
+    }
+
 }
