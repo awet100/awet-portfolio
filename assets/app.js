d25c8d5eb29222d029280c4e12c43c0d6b1ff7da
Awet Tesfay <whitezscope@gmail.com>
2021-05-06 09:22:45 +0200

    toggle the dashbar to display the navbars

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,22 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+// listen to the collapse button
+
+const element = document.getElementById("navbarSupportedContent-4");
+const btn = document.getElementById('displayBtn');
+let state = false;
+
+btn.addEventListener('click', function() {
+    state = !state;
+    if (state) {
+        element.classList.remove('text-center', 'bg-secondary');
+        element.classList.add('collapse');
+    } else {
+        element.classList.add('text-center', 'bg-secondary');
+        element.classList.remove('collapse');
+    }
+});
+
